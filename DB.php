ee381e3de7e18825d618b1bb5326d4a5cd9528fc
natalianr <54320421+natalianr@users.noreply.github.com>
2019-08-27 11:43:17 -0300

    Add files via upload

---
+++ DB.php
@@ -0,0 +1,53 @@
+<?php
+class BD {
+    private $DB_NOME = "db_tai";
+    private $DB_USUARIO = "root";
+    private $DB_SENHA = "";
+    private $DB_CHARTSET = "utf8";
+
+    public function connection(){
+        $str_conn = "mysql:host=localhost;dbname=".$this->DB_NOME;
+
+        return new PDO($str_conn, $this->DB_USUARIO, $this->DB_SENHA,
+        array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES " . $this->DB_CHARSET));
+    }
+
+    public function select(){
+        $conn = $this->connection();
+        $stmt = $conn->prepare("SELECT * FROM tb_alunos LIMIT 3");
+        $stmt->execute;
+        return $stmt;
+    }
+    public function insert($dados){
+        $sql = "INSERT INTO tb_alunos(nome, curso, turma) VALUES(";
+
+        $flag = 0;
+        $arrayValue = [];
+        foreach ($dados as $valor) {
+            if ($flag == 0) {
+                $sql .= "?";
+                $flag = 1;
+            } else {
+                $sql .= ", ?";
+            }
+            $arrayValue[] = $valor;
+        }
+        $sql .= ");";
+
+        $conn = $this->connection();
+        $stmt = $conn->prepare($sql);
+
+        $stmt->execute($arrayValue);
+
+        return $stmt;
+    }
+}
+$dados = array("nome" => "MARCOS",
+    "curso" => "INFORMÁTICA - EMI",
+    "turma" => "INFO14");
+
+$obj = new BD();
+
+$aluno = $obj->insert($dados);
+echo "INSERIDO COM SUCESSO";
+?>
